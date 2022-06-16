4a6c6d9806f6edddf8963a25caac7a18e29a5976
GennadiiIurkevych <yurgensport@gmail.com>
2022-06-16 19:02:54 +0300

    header

---
+++ main-project/src/header.js
@@ -0,0 +1,18 @@
+import Logo from './logo.svg';
+
+
+function Header () {
+  return (
+      <div className='header'>       
+           <a href=" "><img className='logo' src={ Logo } alt="" /></a>               
+        <nav className='nav'>         
+            <button className='button'>TV Shows</button>
+            <button className='button'>Suggest me <svg className='arrow' xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
+            </svg></button>      
+        </nav>  
+      </div>
+  );
+}
+
+export default Header;
