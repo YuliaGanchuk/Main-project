7a4a94a9f0713e53ae4f835452fced50d329756e
Oleg Zaharyak <oleg.zaharyak@gmail.com>
2022-06-16 23:15:20 +0300

    finish route

--- main-project/src/App.js
+++ main-project/src/App.js
@@ -1,11 +1,20 @@
+import { Routes, Route } from 'react-router-dom';
 import './App.scss';
+import { Home } from "./pages/home/Home";
+import { ShowDetail } from "./pages/showDetail/ShowDetail";
+import { SuggestMe } from "./pages/suggestMe/SuggestMe";
 
 function App() {
   return (
-    <>
-      <h>Hello</h>
-    </>
+    <div>
+      <Routes>
+        <Route path="/" element = {<Home/>}/>
+        <Route path="/showDetail" element = {<ShowDetail/>}/>
+        <Route path="/suggestMe" element = {<SuggestMe/>}/>  
+      </Routes>
+    </div>
   );
 }
 
 export default App;
+ 
