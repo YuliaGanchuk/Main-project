08bf31fd89c28111233d77df0bd239e843cab92a
GennadiiIurkevych <yurgensport@gmail.com>
2022-06-17 22:46:30 +0300

    begining

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-
 import Header from './components/Header/header.js';
 import { Routes, Route } from 'react-router-dom';
 import './App.scss';
 import { Home } from "./pages/home/Home";
 import { ShowDetail } from "./pages/showDetail/ShowDetail";
 import { SuggestMe } from "./pages/suggestMe/SuggestMe";
-
-
 
 
 function App() {
