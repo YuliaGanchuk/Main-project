f788c19d91960d3781db7864f1d235e202bd57b9
Yulia Ganchuk <yulia.ganchuk7@gmail.com>
2022-06-18 20:04:46 +0300

    search_component

--- src/App.js
+++ src/App.js
@@ -3,10 +3,24 @@ import './App.scss';
 import { Home } from "./pages/home/Home";
 import { ShowDetail } from "./pages/showDetail/ShowDetail";
 import { SuggestMe } from "./pages/suggestMe/SuggestMe";
+import {Input} from "./components/Input/Input"
+import { useState } from 'react';
 
 function App() {
+
+  
+  // const [text, setText]= useState("");
+
+  // const onchangeFunc = (event) => {
+  //   setText(event.target.value);
+  // }
+  // const onclickFunc = (event) => {
+  //   setText("");
+  // }
+
   return (
     <div>
+            {/* <Input text={text} onchangeFunc = {onchangeFunc} onclickFunc = {onclickFunc}></Input> */}
       <Routes>
         <Route path="/" element = {<Home/>}/>
         <Route path="/showDetail" element = {<ShowDetail/>}/>
