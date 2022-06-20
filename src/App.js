6012debf9a490cd150aa4291de00187570169588
Oleg Zaharyak <oleg.zaharyak@gmail.com>
2022-06-20 21:59:44 +0300

    Ended pagination

--- src/App.js
+++ src/App.js
@@ -3,10 +3,20 @@ import './App.scss';
 import { Home } from "./pages/home/Home";
 import { ShowDetail } from "./pages/showDetail/ShowDetail";
 import { SuggestMe } from "./pages/suggestMe/SuggestMe";
+import Pengination from "./components/Pagination/pengination.js"
+import { useEffect, useState } from 'react';
+
 
 function App() {
+  // Pengination ----------------------------
+  // const [number, setNumber] = useState(1)
+  // useEffect(()=>{
+  //   console.log(number)
+  // },[number])
+  // ----------------------------------------
   return (
     <div>
+      {/* <Pengination count = {16} currentPage = {number} setNumber = {setNumber}/> */}
       <Routes>
         <Route path="/" element = {<Home/>}/>
         <Route path="/showDetail" element = {<ShowDetail/>}/>
