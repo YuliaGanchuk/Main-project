7960b60bc6d737c3c529b04ba18f645f8cf4390a
Yulia Ganchuk <yulia.ganchuk7@gmail.com>
2022-06-21 18:45:45 +0300

    search_component_formatted_code

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import { Routes, Route } from 'react-router-dom';
-import './App.scss';
+import { Routes, Route } from "react-router-dom";
+import "./App.scss";
 import { Home } from "./pages/home/Home";
 import { ShowDetail } from "./pages/showDetail/ShowDetail";
 import { SuggestMe } from "./pages/suggestMe/SuggestMe";
-import {Input} from "./components/Input/Input"
-import { useState } from 'react';
+
 
 function App() {
 
-  
+
+  //search component--------------------------------------------------------------
   // const [text, setText]= useState("");
 
   // const onchangeFunc = (event) => {
@@ -17,18 +17,17 @@ function App() {
   // const onclickFunc = (event) => {
   //   setText("");
   // }
-
+  //-----------------------------------------------------------
   return (
     <div>
-            {/* <Input text={text} onchangeFunc = {onchangeFunc} onclickFunc = {onclickFunc}></Input> */}
+      {/* <Input text={text} onchangeFunc = {onchangeFunc} onclickFunc = {onclickFunc}></Input> */}
       <Routes>
-        <Route path="/" element = {<Home/>}/>
-        <Route path="/showDetail" element = {<ShowDetail/>}/>
-        <Route path="/suggestMe" element = {<SuggestMe/>}/>  
+        <Route path="/" element={<Home />} />
+        <Route path="/showDetail" element={<ShowDetail />} />
+        <Route path="/suggestMe" element={<SuggestMe />} />
       </Routes>
     </div>
   );
 }
 
 export default App;
- 
