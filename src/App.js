8b76ac96a87c0807f96d3503c0513969ffcf7e4b
Yulia Ganchuk <yulia.ganchuk7@gmail.com>
2022-06-23 00:29:16 +0300

    page404

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.scss";
 import { Home } from "./pages/home/Home";
 import { ShowDetail } from "./pages/showDetail/ShowDetails";
 import { SuggestMe } from "./pages/suggestMe/SuggestMe";
+import {Page404} from "./pages/Page404/Page404"
 
 
 
@@ -26,6 +27,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/showDetail" element={<ShowDetail />} />
         <Route path="/suggestMe" element={<SuggestMe />} />
+        <Route path="*" element={<Page404/>} />
       </Routes>
     </div>
   );
---
+++ src/pages/Page404/Page404.js
@@ -0,0 +1,28 @@
+import React from "react";
+import styles from "./Page404.module.scss";
+import imagePage404 from "./imagePage404.svg";
+import { useNavigate } from "react-router-dom";
+
+export const Page404 = () => {
+  let navigate = useNavigate();
+
+  const routeChange = () => {
+    navigate("../", { replace: true });
+  };
+
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.pageContainer}>
+        <img src={imagePage404} alt="man with film roll"></img>
+        <p className={styles.mainText}>Lost your way?</p>
+        <p className={styles.addText}>
+          Oops! This is awkward. You are looking for something that doesn't
+          actually exist.
+        </p>
+        <button className={styles.buttonHome} onClick={routeChange}>
+          Go Home
+        </button>
+      </div>
+    </div>
+  );
+};
