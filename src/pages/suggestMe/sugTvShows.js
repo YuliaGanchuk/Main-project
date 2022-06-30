6e0a90b614be67d99f30f326ffdcbcb69525981f
GennadiiIurkevych <yurgensport@gmail.com>
2022-06-30 16:32:16 +0300

    Revert "final SuggestMe"
    This reverts commit 08e61b5d32c772f7551a0d04c46ff4fbc60ce9f1.

--- src/pages/suggestMe/sugTvShows.js
+++ src/pages/suggestMe/sugTvShows.js
@@ -1,13 +1,20 @@
 import React, { useState, useEffect} from 'react';
 import MoviesCards from "./sug-movie-cards-list";
+import TvCards from "./skeleton";
+import Items from "./items"
 import styles from './sug-home.module.scss';
+
+
 
 const BECKEND_URL = "https://api.tvmaze.com/";
 export default class TvShows extends React.Component {
   constructor(props) {
     super(props);
+    
     this.url = new URL("shows?page=1", BECKEND_URL);
+   
     this.state = { movies: [], isLoaded: false, count: 0};
+
     this.handleStatusChange = this.handleStatusChange.bind(this);
   }
 
@@ -16,14 +23,14 @@ export default class TvShows extends React.Component {
   }
 
   handleStatusChange(data) {
-    const movieNumber = data.slice(0, 96);  
+    const movieNumber = data.slice(0, 8);
     this.setState({
       movies: movieNumber,
       isLoaded: true,
       count: movieNumber.length
-    }); 
+    });
   }
-  
+
   loadData() {
     fetch(this.url)
       .then((response) => response.json())
@@ -34,7 +41,8 @@ export default class TvShows extends React.Component {
   render() {
     if (this.state.isLoaded) {
       return (
-        <> 
+        <>
+          
           <div className={styles.mainCardsBlock} data-element="cardsList">
             <MoviesCards movies={this.state.movies}></MoviesCards>
           </div>
